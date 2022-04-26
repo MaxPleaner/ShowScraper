43ba82ee7c02a53f2328cfc21a4c6bebc4ba5fa6
max pleaner <max.pleaner@hover.to>
2022-04-26 09:57:38 -0700

    organize

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -2,13 +2,11 @@ require "selenium-webdriver"
 require 'pry'
 require 'active_support/all'
 
+Dir.glob("#{__dir__}/lib/sources/*.rb").each { |path| load path }
+
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless')
 $driver = Selenium::WebDriver.for :chrome, options: options
-
-
-load './selenium_patches.rb'
-
-Dir.glob("./sources/*.rb").each { |path| load path }
+load "#{__dir__}/lib/selenium_patches.rb"
 
 Knockout.run
