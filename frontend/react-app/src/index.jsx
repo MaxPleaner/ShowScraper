157d3781f467f4eea9c3d2b67795eef9a8c8d891
max pleaner <max.pleaner@hover.to>
2022-05-31 11:55:13 -0700

    temp

--- frontend/react-app/src/index.jsx
+++ frontend/react-app/src/index.jsx
@@ -4,10 +4,17 @@ import {App} from './App';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+// let basename;
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+//   basename = "/"
+// } else {
+const basename = "/ShowScraper"
+// }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/ShowScraper">
+    <BrowserRouter basename={basename}>
       <Routes>
         <Route path="/" element={<App route='ListView' />} />
         <Route path="/ListView" element={<App route='ListView' />} />
