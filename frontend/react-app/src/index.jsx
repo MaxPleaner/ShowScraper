74168590f2352995b669c5cb4de7f6f205585972
max pleaner <max.pleaner@hover.to>
2022-05-31 14:12:03 -0700

    temp

--- frontend/react-app/src/index.jsx
+++ frontend/react-app/src/index.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+    <HashRouter basename={`/${process.env.PUBLIC_URL}`}>
       <Routes>
-        <Route exact path="/" element={<App route='ListView' />} />
-        <Route exact path="/ListView" element={<App route='ListView' />} />
-        <Route exact path="/VenuesListView" element={<App route='VenuesListView' />} />
+        <Route path="/" element={<App route='ListView' />} />
+        <Route path="/ListView" element={<App route='ListView' />} />
+        <Route path="/VenuesListView" element={<App route='VenuesListView' />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
