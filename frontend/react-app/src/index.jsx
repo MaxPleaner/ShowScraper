2af29f88a842f158a7f5e1157101fd2dd448bce1
max pleaner <max.pleaner@hover.to>
2022-05-31 19:53:04 -0700

    temp

--- frontend/react-app/src/index.jsx
+++ frontend/react-app/src/index.jsx
@@ -7,9 +7,9 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter basename="https://maxpleaner.github.io/ShowScraper">
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path="/" element={<App route='ListView' />} />
+        <Route exact path="/" element={<App route='ListView' />} />
         <Route path="/ListView" element={<App route='ListView' />} />
         <Route path="/VenuesListView" element={<App route='VenuesListView' />} />
       </Routes>
