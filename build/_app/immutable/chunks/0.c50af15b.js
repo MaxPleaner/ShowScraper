6fb350b9824688392a0648dbd039daf622af3ddc
max pleaner <max.pleaner@hover.to>
2023-03-02 18:06:21 -0800

    un-ignore build

---
+++ build/_app/immutable/chunks/0.c50af15b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/layout.svelte.2fec3526.js";export{t as component};
---
+++ build/_app/immutable/chunks/1.72acf8e4.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.a333a5f4.js";export{t as component};
