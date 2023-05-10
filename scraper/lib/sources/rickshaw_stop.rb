077c8ef8f78604e379795c977c1162e8e96d5c21
max pleaner <max.pleaner@hover.to>
2023-05-10 08:44:47 -0700

    fix chapel and rickshaw stop scrapers

--- scraper/lib/sources/rickshaw_stop.rb
+++ scraper/lib/sources/rickshaw_stop.rb
@@ -10,12 +10,9 @@ class RickshawStop
 
   def self.run(events_limit: self.events_limit, &foreach_event_blk)
     $driver.get(MAIN_URL)
-    index = 0
-    get_events.map do |event|
+    get_events.map.with_index do |event, index|
       next if index >= events_limit
-      result = parse_event_data(event, &foreach_event_blk)
-      index += 1 if result
-      result
+      parse_event_data(event, &foreach_event_blk)
     end.compact
   end
 
@@ -23,27 +20,30 @@ class RickshawStop
     private
 
     def get_events
-      $driver.css(".calendar-day-event").select do |event|
-        event.css("#event_tickets").any?
-      end
+      # resize the window so we get the better-organized calendar view
+      max_width, max_height = $driver.execute_script("return [window.screen.availWidth, window.screen.availHeight];")
+      $driver.manage.window.resize_to(2000, max_height)
+
+      $driver.css(".seetickets-calendar-event-container")
     end
 
     def parse_event_data(event, &foreach_event_blk)
-      link = event.css("#event_tickets")[0].attribute("href")
-      date = DateTime.parse(event.css(".value-title")[0].text.gsub(".", "/"))
-      img = event.css(".detail_seetickets_image img")[0].attribute("src")
-      title = event.css(".event-title")[0].text
       {
-        date: date,
-        img: img,
-        title: title,
-        url: link,
+        date: parse_date(event),
+        img: event.css(".seetickets-calendar-event-picture img")[0].attribute("src"),
+        title: event.css(".title")[0].text,
+        url: event.css(".seetickets-calendar-event-picture")[0].attribute("href"),
         details: ""
       }.
-        tap { |data| Utils.print_event_preview(self, data) if data }.
-        tap { |data| foreach_event_blk&.call(data) if data }
+        tap { |data| Utils.print_event_preview(self, data) }.
+        tap { |data| foreach_event_blk&.call(data) }
     rescue => e
       ENV["DEBUGGER"] == "true" ? binding.pry : raise
+    end
+
+    def parse_date(event)
+      month, day = event.css(".seetickets-buy-btn a")[0].attribute("aria-label").split(" ")[-2..-1]
+      DateTime.parse("#{month}/#{day}")
     end
   end
 end
--- scraper/lib/sources/the_chapel.rb
+++ scraper/lib/sources/the_chapel.rb
@@ -1,52 +1,36 @@
 class TheChapel
-  # This scraper was recently updated from a calendar view to a paginated view.
-  # Even though the URL says "calendar" it's not really a calendar.
-
-  MAIN_URL = "https://thechapelsf.com/calendar/?sepage="
+  # Awesome calendar which shows multiple months at once!
+  MAIN_URL = "https://thechapelsf.com/calendar/"
 
   cattr_accessor :events_limit, :load_time
   self.events_limit = 200
   self.load_time = 3
 
   def self.run(events_limit: self.events_limit, &foreach_event_blk)
-    events = []
-    i = 1
-    get_page(i)
-
-    loop do
-      get_events.each do |event|
-        next if events.count >= events_limit
-        events.push(parse_event_data(event, &foreach_event_blk))
-      end
-      break if events.count >= events_limit
-      i += 1
-      got_next_page = get_page(i)
-      break unless got_next_page
-    end
-
-    events.compact
+    $driver.get(MAIN_URL)
+    get_events.map.with_index do |event, index|
+      next if index >= events_limit
+      parse_event_data(event, &foreach_event_blk)
+    end.compact
   end
 
   class << self
     private
 
-    def get_page(i)
-      $driver.get(MAIN_URL + i.to_s)
-    end
-
     def get_events
-      $driver.css(".grid-item")
-      # $driver.css(".calendar-day").select do |elem|
-      #   elem.css("#event_tickets").any?
-      # end
+      # resize the window so we get the better-organized calendar view
+      max_width, max_height = $driver.execute_script("return [window.screen.availWidth, window.screen.availHeight];")
+      $driver.manage.window.resize_to(2000, max_height)
+
+      $driver.css(".seetickets-calendar-event-container")
     end
 
     def parse_event_data(event, &foreach_event_blk)
       {
         date: parse_date(event),
-        img: event.css(".seetickets-list-view-event-image")[0].attribute("src"),
+        img: event.css(".seetickets-calendar-event-picture img")[0].attribute("src"),
         title: event.css(".title")[0].text,
-        url: event.css(".title a")[0].attribute("href"),
+        url: event.css(".seetickets-calendar-event-picture")[0].attribute("href"),
         details: ""
       }.
         tap { |data| Utils.print_event_preview(self, data) }.
@@ -56,8 +40,7 @@ class TheChapel
     end
 
     def parse_date(event)
-      date_str = event.css(".date")[0].text
-      month, day = date_str.split(" ")[1..2]
+      month, day = event.css(".seetickets-buy-btn a")[0].attribute("aria-label").split(" ")[-2..-1]
       DateTime.parse("#{month}/#{day}")
     end
   end
