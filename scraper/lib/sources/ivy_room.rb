9bf710eddb87a8d8f881972b70d8480cc1b129da
max pleaner <max.pleaner@hover.to>
2023-05-11 09:34:26 -0700

    fix ivy room scraper

--- scraper/lib/sources/ivy_room.rb
+++ scraper/lib/sources/ivy_room.rb
@@ -2,20 +2,16 @@ class IvyRoom
   MAIN_URL = "https://www.ivyroom.com/calendar?view=calendar"
 
   cattr_accessor :months_limit, :events_limit, :load_time
-  self.months_limit = 3
+  self.months_limit = 1
   self.events_limit = 200
   self.load_time = 2
 
   def self.run(events_limit: self.events_limit, &foreach_event_blk)
     events = []
     $driver.get(MAIN_URL)
-    months_limit.times do |i|
-      get_events.each do |event|
-        next if events.count >= events_limit
-        events.push(parse_event_data(event, &foreach_event_blk))
-      end
-      break if events.count >= events_limit
-      get_next_page unless i == months_limit - 1
+    get_events.each do |event|
+      next if events.count >= events_limit
+      events.push(parse_event_data(event, &foreach_event_blk))
     end
     events
   end
@@ -25,33 +21,28 @@ class IvyRoom
 
     def get_events
       sleep load_time
-      $driver.css(".background-image-link")
-    end
-
-    def get_next_page
-      $driver.css(".yui3-calendarnav-nextmonth")[0].click
+      $driver.css(".event-card")
     end
 
     def parse_event_data(event, &foreach_event_blk)
-      $driver.new_tab(event.attribute("href")) do
-        {
-          url: $driver.current_url,
-          img: $driver.css(".image-block img")[0].attribute("src"),
-          date: parse_date,
-          title: $driver.title.gsub("IVY ROOM - ", ""),
-          details: ""
-        }
-      end.
+      {
+        # links are done here through modals, not standalone links
+        url: $driver.current_url,
+        img: parse_img(event),
+        date: DateTime.parse(event.css(".time-info")[0].text),
+        title: [".event-name", ".support"].map { |c| event.css(c)[0].text }.compact.join(", "),
+        details: ""
+      }.
         tap { |data| Utils.print_event_preview(self, data) }.
         tap { |data| foreach_event_blk&.call(data) }
     rescue => e
       ENV["DEBUGGER"] == "true" ? binding.pry : raise
     end
 
-    def parse_date
-      date_parts = $driver.current_url.split("/")[-1].chars.each_slice(2).map(&:join)
-      date_str = "#{date_parts[1]}-#{date_parts[0]}-20#{date_parts[2]}"
-      DateTime.parse(date_str)
+    def parse_img(event)
+      style = event.css(".main-img")[0].attribute("style")
+      style.split("background-image: url(\"")[1].split("\");")[0]
     end
+
   end
 end
