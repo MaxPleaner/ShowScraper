0afca91492c9d20f5896ae82b97ab2458cfdc23c
max pleaner <max.pleaner@hover.to>
2023-11-28 10:47:24 -0800

    update milk scraper?

--- scraper/lib/sources/milk_bar.rb
+++ scraper/lib/sources/milk_bar.rb
@@ -15,7 +15,7 @@ class MilkBar
       result = parse_event_data(event, &foreach_event_blk)
       events.push(result) if result.present?
     end
-    events.uniq { |e| [e[:date], e[:title]] }
+    events.uniq { |e| [e[:date], e[:title]] }.sort_by { |e| e[:date] }
   end
 
   class << self
