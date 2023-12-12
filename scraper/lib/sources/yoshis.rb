4b1e30b47f585150991b3ab2a4604c53ddffa44d
max pleaner <max.pleaner@hover.to>
2023-12-11 21:59:27 -0800

    fix yoshis scraper

--- scraper/lib/sources/yoshis.rb
+++ scraper/lib/sources/yoshis.rb
@@ -15,18 +15,20 @@ class Yoshis
   self.events_limit = 200
 
   def self.run(events_limit: self.events_limit, &foreach_event_blk)
-    events = []
+    results = []
     $driver.get(MAIN_URL)
     months_limit.times do |i|
-      get_events.each do |event|
+      events = get_events
+      events.each do |event|
         next if events.count >= events_limit
-        events.push(parse_event_data(event, &foreach_event_blk))
+        result = parse_event_data(event, &foreach_event_blk)
+        results.push(*result)
       end
-      break if events.count >= events_limit
+      break if results.count >= events_limit
       got_next_page = get_next_page unless i == months_limit - 1
       break unless got_next_page
     end
-    events.compact
+    results.compact
   end
 
   class << self
@@ -58,15 +60,18 @@ class Yoshis
 
     def parse_event_data(event, &foreach_event_blk)
       return unless event.css(".eimage a")[0]
-      {
-        url: event.css(".eimage a")[0].attribute("href"),
-        img: event.css(".eimage img")[0].attribute("src"),
-        date: (parse_date(event) rescue return),
-        title: parse_title(event),
-        details: ""
-      }.
-      tap { |data| Utils.print_event_preview(self, data) }.
-      tap { |data| foreach_event_blk&.call(data) }
+      dates = parse_date(event) rescue return
+      dates.map do |date|
+        {
+          url: event.css(".eimage a")[0].attribute("href"),
+          img: event.css(".eimage img")[0].attribute("src"),
+          date: date,
+          title: parse_title(event),
+          details: ""
+        }.
+        tap { |data| Utils.print_event_preview(self, data) }.
+        tap { |data| foreach_event_blk&.call(data) }
+      end
     rescue => e
       ENV["DEBUGGER"] == "true" ? binding.pry : raise
     end
@@ -78,9 +83,42 @@ class Yoshis
     end
 
     def parse_date(event)
-      date_str = event.css(".edate")[0].text
-      month, day = date_str.split("\n")[0].scan(/(\d+)\.(\d+)/)[0]
-      DateTime.parse("#{month}/#{day}")
+      date_str = event.css(".edate")[0].text.tr("\t\n", " ")
+      parse_date_range(date_str.strip)
+      # month, day = date_str.split("\n")[0].scan(/(\d+)\.(\d+)/)[0]
+      # DateTime.parse("#{month}/#{day}")
     end
+    require 'date'
+
+    def parse_date_range(date_range_str)
+      if !date_range_str.include?(' to ')
+        date = date_range_str.match(/\d+\.\d+/)[0]
+        month, day = date.split('.').map(&:to_i)
+        current_year = Date.today.year
+        return [Date.new(current_year, month, day)]
+      end
+
+      # Splitting the string to extract the start and end dates
+      start_str, end_str = date_range_str.split(' to ').map(&:strip).map do |str|
+        str.match(/\d+\.\d+/)[0]
+      end
+    
+      # Extracting the month and day from the date strings
+      start_month, start_day = start_str.split('.').map(&:to_i)
+      end_month, end_day = end_str.split('.').map(&:to_i)
+    
+      # Assuming the year is the current year (you may need to adjust this)
+      current_year = Date.today.year
+    
+      # Handling year change if the end month is earlier in the year than the start month
+      end_year = end_month < start_month ? current_year + 1 : current_year
+    
+      # Creating Date objects
+      start_date = Date.new(current_year, start_month, start_day)
+      end_date = Date.new(end_year, end_month, end_day)
+    
+      # Generating the range of dates
+      (start_date..end_date).to_a
+    end    
   end
 end
