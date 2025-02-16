8e3506b1b72b5300bc775d9aa3d83481a11929a2
max pleaner <maxpleaner@gmail.com>
2025-02-16 09:05:45 -0800

    fix rickshow stop

--- scraper/lib/sources/rickshaw_stop.rb
+++ scraper/lib/sources/rickshaw_stop.rb
@@ -1,7 +1,5 @@
 class RickshawStop
   # Awesome calendar which shows multiple months at once!
-  # However it ended up not being easy to scrape because it uses multiple
-  # hosts for detail pages.
   MAIN_URL = "https://rickshawstop.com/calendar/"
 
   cattr_accessor :events_limit, :load_time
@@ -10,9 +8,9 @@ class RickshawStop
 
   def self.run(events_limit: self.events_limit, &foreach_event_blk)
     $driver.get(MAIN_URL)
-    get_events.map.with_index do |event, index|
+    get_events.map.with_index do |(month_node, event), index|
       next if index >= events_limit
-      parse_event_data(event, &foreach_event_blk)
+      parse_event_data(month_node, event, &foreach_event_blk)
     end.compact
   end
 
@@ -23,27 +21,29 @@ class RickshawStop
       # resize the window so we get the better-organized calendar view
       max_width, max_height = $driver.execute_script("return [window.screen.availWidth, window.screen.availHeight];")
       $driver.manage.window.resize_to(2000, max_height)
-
-      $driver.css(".seetickets-calendar-event-container")
+      month_groups = $driver.css(".seetickets-calendar")
+      month_headers = $driver.css(".seetickets-calendar-year-month-container")
+      month_groups.flat_map.with_index do |month_group, idx|
+        events = month_group.css(".seetickets-calendar-event-container")
+        month = month_headers[idx]
+        events.map { |event| [month, event] }
+      end
     end
 
-    def parse_event_data(event, &foreach_event_blk)
+    def parse_event_data(month_node, event, &foreach_event_blk)
+      month, year = month_node.text.split(" ")
+      day = event.parent.css(".date-number")[0].text.to_i
       {
-        date: parse_date(event),
-        img: event.css(".seetickets-calendar-event-picture img")[0].attribute("src"),
+        date: DateTime.parse("#{month} #{day}, #{year}"),
+        img: event.css(".seetickets-calendar-event-picture").map { |x| x.attribute("src") }.compact.first,
         title: event.css(".title")[0].text,
-        url: event.css(".seetickets-calendar-event-picture")[0].attribute("href"),
+        url: event.css(".title a")[0].attribute("href"),
         details: ""
       }.
         tap { |data| Utils.print_event_preview(self, data) }.
         tap { |data| foreach_event_blk&.call(data) }
     rescue => e
       ENV["DEBUGGER"] == "true" ? binding.pry : raise
-    end
-
-    def parse_date(event)
-      month, day = event.css(".seetickets-buy-btn a")[0].attribute("aria-label").split(" ")[-2..-1]
-      DateTime.parse("#{month}/#{day}")
     end
   end
 end
