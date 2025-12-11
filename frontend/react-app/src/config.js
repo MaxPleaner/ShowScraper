cd8e8c484123ad21e5d0801fc3fe2bc772918060
maxpleaner <maxpleaner@gmail.com>
2025-12-11 11:35:41 -0800

    fix path

--- frontend/react-app/src/config.js
+++ frontend/react-app/src/config.js
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
   USE_SAMPLE_DATA: false,
   CONCERT_RESEARCH_ENDPOINT: process.env.NODE_ENV === 'production'
-    ? 'https://llm-backend.dissoannt.info/tasks/concert-research'
+    ? 'https://llm-backend.dissonant.info/tasks/concert-research'
     : 'http://localhost:8000/tasks/concert-research'
 };
