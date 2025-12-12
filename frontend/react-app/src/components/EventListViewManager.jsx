cc741adf45d53332771c8212b5d15276cf55046e
maxpleaner <maxpleaner@gmail.com>
2025-12-11 16:21:31 -0800

    update text

--- frontend/react-app/src/components/EventListViewManager.jsx
+++ frontend/react-app/src/components/EventListViewManager.jsx
@@ -130,8 +130,8 @@ export default class EventListViewManager extends React.Component {
       <div className='ListViewManager'>
 
           <div className='ai-integration-notice'>
-            <span className='notice-label'>NOTICE!</span>
-            <span> Techies have hacked the site and now there's an AI integration. Press the </span>
+            <span className='notice-label'>NOTICE:</span>
+            <span> There's now an AI integration. Press the </span>
             <span className='ai-icon-example'>🤖</span>
             <span> icon to attempt to get info about the bands</span>
           </div>
